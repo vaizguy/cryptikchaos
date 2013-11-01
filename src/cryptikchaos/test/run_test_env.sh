78a75dce311977d7c71fc6e934a496e7f82bc2aa
Arun Vaidya <vaizguy@gmail.com>
2013-11-01 19:43:33 +0530

    Added inbuilt support for profiling client as well as ability to generate call graph based on cProfiler output.

--- src/cryptikchaos/test/run_test_env.sh
+++ src/cryptikchaos/test/run_test_env.sh
@@ -1,3 +1,37 @@
-rm -rf ../db/*_db
-xterm -geometry 96x24+0+0 -e 'sudo python ./test_server.py' &
-xterm -geometry 96x24+0-0 -e 'sudo python ../core/main.py'
+#! /bin/bash
+
+## Path to profile database
+PROFILE_PATH="../../../profile"
+
+## For Profiling
+if [[ $1 =~ "profile" ]]; then
+    # Remove client database
+    rm -rf ../db/*_db
+
+    # Launch test server
+    xterm -geometry 96x24+0-0 -e \
+        'sudo python ./test_server.py' &
+
+    # Launch the main client
+    xterm -geometry 96x24+0+0 -e \
+        'python -m cProfile -o \
+        ${PROFILE_PATH}/profile.pstats -s time ../core/main.py'
+
+    # Convert Profile statistics to a graph diag.
+    ${PROFILE_PATH}/gprof2dot.py \
+        -f pstats ${PROFILE_PATH}/profile.pstats \
+        | dot -Tsvg -o ${PROFILE_PATH}/callgraph.svg
+
+## No profiling
+else
+    # Remove client database    
+    rm -rf ../db/*_db
+
+    # Launch test server
+    xterm -geometry 96x24+0+0 -e 'sudo python ./test_server.py' &
+
+    # Launch the main client
+    xterm -geometry 96x24+0-0 -e 'sudo python ../core/main.py'
+fi
+
+
