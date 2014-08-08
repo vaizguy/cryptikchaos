f556f0ed90c739357dc99db546c6e5c99d280608
Arun Vaidya <vaizguy@gmail.com>
2014-08-08 20:11:25 +0530

    Adding missing pkg

---
+++ src/cryptikchaos/storage/manager.py
@@ -0,0 +1,152 @@
+'''
+Created on Nov 3, 2013
+
+@author: vaizguy
+'''
+
+from kivy.logger import Logger
+
+from cryptikchaos.storage.store import Store
+
+from cryptikchaos.libs.Table.prettytable import PrettyTable
+
+
+class StoreManager(object):
+
+    def __init__(self, name, valid_keys):
+        "Initialize store manager."
+
+        Logger.info("Opening [{}] store manager.".format(
+            name
+        ))
+
+        # Store attributes
+        self._storage = {}
+        self._name = name
+        self._store_keys = valid_keys
+
+    def __str__(self):
+        "Store as string."
+
+        return str(self._storage)
+
+    def __repr__(self):
+        "Store representation."
+
+        return "StoreManager({})".format(self._storage)
+
+    def __del__(self):
+        "Delete store contents"
+
+        try:
+            del self._name
+            del self._storage
+            del self._name
+            del self._store_keys
+        except:
+            pass
+
+    def keys(self):
+        "Return the storage index keys."
+
+        return self._storage.keys()
+
+    def in_store(self, sid):
+        "Check if store id exists."
+
+        return sid in self.keys()
+
+    def add_store(self, sid, dictionary):
+        "Add a new store."
+
+        self._storage[sid] = Store(self._store_keys, dictionary)
+
+    def delete_store(self, sid):
+        "Delete store."
+
+        if self.in_store(sid):
+            del self._storage[sid]
+        else:
+            return None
+
+    def get_store(self, sid):
+        "Return the store."
+
+        if self.in_store(sid):
+            return self._storage[sid]
+        else:
+            return None
+
+    def set_store_item(self, sid, key, value):
+        "Set item in existing store."
+
+        try:
+            _dict = self._storage[sid]
+        except KeyError:
+            raise Exception(
+                "Store needs to be created before being modified."
+            )
+        else:
+            _dict[key] = value
+            self._storage[sid] = _dict
+
+    def get_store_item(self, sid, key):
+        "Return item from store."
+
+        if self.in_store(sid):
+            _dict = self._storage[sid]
+            return _dict[key]
+        else:
+            return None
+
+    def storage_table(self):
+        "Display Store in table format."
+
+        if not self._storage.keys():
+            return None
+
+        table = PrettyTable(["ID"] + list(self._store_keys))
+
+        for sid in self._storage.keys():
+
+            row = [sid]
+
+            _dict = self._storage[sid]
+
+            for k in _dict.keys():
+                # get value
+                v = _dict[k]
+                # Check on length
+                if (len(str(v)) <= 8):
+                    row += [v]
+                else:
+                    row += ["{}..".format(v[:8])]
+
+            table.add_row(row)
+
+        return table
+
+    def get_store_hmac(self, sid):
+        "Get stores checksum"
+
+        return self._storage[sid].hmac()
+
+    def check_hmac(self, sid, hmac):
+        "Validate store checksum"
+
+        return self._storage[sid].check_hmac(hmac)
+
+
+if __name__ == "__main__":
+
+    k = ["key1", "key2", "key3"]
+
+    sm = StoreManager("StoreTest", k)
+
+    sm.add_store(1, {"key1": 1, "key2": 2, "key3": 3})
+    sm.add_store(2, {"key1": 4, "key2": 5, "key3": 6})
+    sm.add_store(3, {"key1": 7, "key2": 8, "key3": 9})
+    sm.add_store(4, {"key1": 'a', "key2": 'b', "key3": 'c'})
+    print sm
+
+    print sm.storage_table()
---
+++ src/cryptikchaos/storage/store.py
@@ -0,0 +1,126 @@
+'''
+Created on Nov 3, 2013
+
+@author: vaizguy
+'''
+
+from cryptikchaos.libs.customtypes import TransformedDict
+from cryptikchaos.libs.utilities import md5hash
+import hmac
+import exceptions
+
+
+class Store(TransformedDict):
+
+    "custom store"
+
+    def __init__(self, keys, dictionary):
+        "Initialize dictionary."
+
+        # autherized keys
+        self._valid_keys = keys
+
+        # Init parent
+        super(Store, self).__init__(**dictionary)
+
+    def __str__(self):
+        "String equivalent."
+
+        return TransformedDict.__str__(self)
+
+    def __repr__(self):
+        "Object representation as string."
+
+        return "Store({})".format(TransformedDict.__repr__(self))
+
+    def __dict__(self):
+        "Return dictionary obj"
+
+        return TransformedDict.__dict__()
+
+    def __getitem__(self, key):
+        "Get value from dictionary."
+
+        # Get item
+        return TransformedDict.__getitem__(self, key)
+
+    def __setitem__(self, key, value):
+        "Set key value-pair in dictionary"
+
+        if key in self._valid_keys:
+            return TransformedDict.__setitem__(self, key, value)
+        else:
+            raise Exception("Error: Invalid peer attribute '{}'.".format(key))
+
+    def __delitem__(self, key):
+        "Reset key's value."
+
+        if key in self._valid_keys:
+            # Delete the item
+            TransformedDict.__delitem__(self, key)
+            # Re-create key with no value
+            TransformedDict.__setitem__(self, key, None)
+        else:
+            # Delete unauth key
+            TransformedDict.__delitem__(self, key)
+
+    def __keytransform__(self, key):
+        "Manipulate key"
+
+        return self._hash_key(key)
+
+    def _hash_key(self, key):
+        "Return md5 hash of key"
+
+        return md5hash(key)
+
+    def keys(self):
+        "Return list of keys"
+
+        return self._valid_keys
+
+    def iteritems(self):
+        "Iter (key, value) tuples"
+
+        return [(k, self.__getitem__(k)) for k in self._valid_keys]
+
+    def values(self):
+        "Return list of values"
+
+        return [self.__getitem__(k) for k in self._valid_keys]
+
+    def items(self):
+        "iteritem alias"
+
+        return [(k, v) for (k, v) in self.iteritems()]
+
+    def hmac(self):
+        "Get Store checksum"
+
+        _hmac = hmac.new(self.__repr__())
+
+        for key in self._valid_keys:
+            _hmac.update(str(self.__getitem__(key)))
+
+        return _hmac.hexdigest()
+
+    def check_hmac(self, _hmac):
+        "HMAC challenge"
+
+        try:
+            return hmac.compare_digest(self.hmac(), _hmac)
+        except exceptions.AttributeError:
+            return self.hmac() == _hmac
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    store = Store(keys=["key1", "key2"], dictionary={"key1": 1, "key2": 2})
+    print store
+    print store["key1"]
+    del store["key2"]
+    print store
+    print store.__repr__()
+    import cPickle
+    print cPickle.loads(cPickle.dumps(store, protocol=0)) == store
