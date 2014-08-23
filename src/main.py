fab41ee5abcd44ee581c1efb5e1c55e808c771b3
Arun Vaidya <vaizguy@gmail.com>
2014-08-24 02:40:11 +0530

    better error handling for inturrupts during app start.

--- src/main.py
+++ src/main.py
@@ -12,13 +12,31 @@ main.py is the kivy app entry file.
 
 __author__ = "Arun Vaidya"
 __version__ = "0.6"
-      
-if __name__ == '__main__':
+
+import atexit
+from kivy.logger import Logger
+
+@atexit.register
+def on_close():
+    Logger.info("Exiting python.")
+
+def main():
     # Add cryptikchaos path
     import pythonpath
     pythonpath.AddSysPath('.') # ./main.py (pwd: src)
     pythonpath.AddSysPath('../..') # ../../main.py (pwd: test dir)
     
-    from cryptikchaos import CryptikChaosApp
-    from cryptikchaos.libs.utilities import run_kivy_app
-    run_kivy_app(CryptikChaosApp)
+    try:
+        from cryptikchaos import CryptikChaosApp
+    except KeyboardInterrupt:
+        Logger.error("Cryptikchaos services was interrupted.")
+    except ImportError:
+        Logger.error("Please check if the package `cryptikchaos.CryptikChaosApp` exists.")
+    else:
+        from cryptikchaos.libs.utilities import run_kivy_app
+        run_kivy_app(CryptikChaosApp)
+
+        
+if __name__ == '__main__':
+    main()
+        
